b5d24e9b22af80883afa58e26e125f4bcff3ed41
MARCANDREPC\Marc-André <marcandrechabot86@gmail.com>
2018-07-05 23:07:11 -0400

    oups oublie d'ajouter TEF

---
+++ IFT2255 - Projet1/src/TEF.java
@@ -0,0 +1,14 @@
+
+public class TEF {
+	String nomDuPro;
+	Service service;
+	int numeroPro;
+	int nombreInscrit;
+	
+	public TEF(String pro, Service service,int  numeroPro,int nombreInscrit) {
+		this.nomDuPro= pro;
+		this.service = service;
+		this.numeroPro = numeroPro;
+		this.nombreInscrit= nombreInscrit;
+	}
+}
