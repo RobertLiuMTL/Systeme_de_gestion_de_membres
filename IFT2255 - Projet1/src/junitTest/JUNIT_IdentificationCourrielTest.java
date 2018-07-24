b52a99816198686a90e8095d6787c1890c6f31f2
Robert Liu <robert.liu1987@gmail.com>
2018-07-23 23:19:17 -0400

    Test JUnit pour dataCenter et Services/Séances

---
+++ IFT2255 - Projet1/src/junitTest/JUNIT_IdentificationCourrielTest.java
@@ -0,0 +1,14 @@
+package junitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUNIT_IdentificationCourrielTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
